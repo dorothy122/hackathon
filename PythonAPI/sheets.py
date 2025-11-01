389e394f1a70769cb5ef56bca7cf715d61526876
Bethany Hamer <111655809+student2208@users.noreply.github.com>
2025-11-01 20:58:42 +0000

    adding link to sheets

--- PythonAPI/sheets.py
+++ PythonAPI/sheets.py
@@ -1,11 +1,14 @@
-import gspread
+import os
 from oauth2client.service_account import ServiceAccountCredentials
+import gspread
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+CREDENTIALS_PATH = os.path.join(BASE_DIR, "credentials.json")
 
 scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-credentials = ServiceAccountCredentials.from_json_keyfile_name("cred.json", scope)
+credentials = ServiceAccountCredentials.from_json_keyfile_name(CREDENTIALS_PATH, scope)
 
 client = gspread.authorize(credentials)
 sheet = client.open("My Spreadsheet").sheet1
 
-first_row = sheet.row_values(3)
-print(first_row)
+print(sheet.row_values(3))
