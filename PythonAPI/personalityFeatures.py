5044b2ca7868a9baea0a28908e846d5aa3627c64
Bethany Hamer <111655809+student2208@users.noreply.github.com>
2025-11-01 22:46:06 +0000

    Update personalityFeatures.py

--- PythonAPI/personalityFeatures.py
+++ PythonAPI/personalityFeatures.py
@@ -1,4 +1,7 @@
+from flask import Flask, jsonify
 from sheets import connect_sheet
+
+app = Flask(__name__)
 
 sheet =  connect_sheet("My Spreadsheet")   
 records = sheet.get_all_records()
@@ -7,14 +10,34 @@ lastRecord = records[-1]
 
 twins = False
 married = False
+rich = True
+moveCountries = False
 
 if(lastRecord["Gender"] == "F" and lastRecord["Height"] > 164):
     twins = True
 elif(lastRecord["Height"] > 180):
     married = True
 
-careers = {"january": "Athlete", "febuary":"artist", "march":"pilot", "april":"dictator", "may":"Politian", "june":"CEO", "july":"bricklayer","august":"hairdresser", "september":"academic", "october":"lawyer", "november":"serial killer","december":"dentist"}
+careers = {"january": "an athlete", "febuary":"an artist", "march":"a pilot", "april":"a dictator", "may":"a politian", "june":"a CEO", "july":"a bricklayer","august":"a hairdresser", "september":"an academic", "october":"a lawyer", "november":"a serial killer","december":"a dentist"}
 
 career = careers[lastRecord["BirthMonth"].lower()]
 
+if(lastRecord["Drive"] == "No" or lastRecord["Glasses"] == "Yes"):
+    rich = True
 
+if(lastRecord["Tounge"] == "Yes"):
+    moveCountries = True
+
+def get_results():
+    return jsonify(
+    {
+        'twins': twins,
+        'married': married,
+        'career': career,
+        'rich': rich,
+        'moveCountries': moveCountries
+    }
+)
+
+if __name__ == '__main__':
+    app.run(debug=True)
